6b20b6f47ebbf6bbbee9618322e07f795a0f6d87
winttheingihlaing <winttheingihlaing@gmail.com>
2018-06-25 19:48:50 +0630

    Strings and Text

--- python-exercises/ex6.py
+++ python-exercises/ex6.py
@@ -14,7 +14,7 @@ print(f"I also said: '{y}'")
 hilarious = False
 joke_evaluation = "Isn't that joke so funny? ! {}"
 
-print("joke_evaluation.gormat(hilarious)")
+print(joke_evaluation.format(hilarious))
 
 w = "This is the left side of..."
 e = "a string with a right side."
